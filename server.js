48f638e7257d007701ce3a726c630f1d58b5fcf5
= <=>
2018-08-06 01:14:10 +0530

    deleted package.json

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/upstox/realtime-dashboard'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/upstox/realtime-dashboard/index.html'));
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/upstox/realtime-dashboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
